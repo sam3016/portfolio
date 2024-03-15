90b0067577818ae44dd0b75e673be814d8612c2d
Sam H <samh007@tutanota.com>
2024-03-15 23:18:50 +0000

    Update

--- src/app/certification/page.tsx
+++ src/app/certification/page.tsx
@@ -8,22 +8,7 @@ import {
   TableRow,
 } from "@/components/ui/table";
 import Image from "next/image";
-
-import ccexpert from "../../../public/certification/app-developer-for-claris-connect-expert.png";
-import fmps from "../../../public/certification/app-developer-for-claris-filemaker-pro-specialist.png";
-import fmsa from "../../../public/certification/claris-filemaker-server-administrator-associate.png";
-import ccs from "../../../public/certification/app-developer-for-claris-connect-specialist.png";
-import fmpa from "../../../public/certification/app-developer-for-claris-filemaker-pro-associate.png";
-import cca from "../../../public/certification/app-developer-for-claris-connect-associate.png";
-import tsa from "../../../public/certification/tableau-server-certified-associate.png";
-import da from "../../../public/certification/tableau-certified-data-analyst.1.png";
-import tcc from "../../../public/certification/tableau-certified-consultant.1.png";
-import az900 from "../../../public/certification/microsoft-certified-azure-fundamentals.png";
-import defmp from "../../../public/certification/developer-essentials-for-claris-filemaker.png";
-import fm17 from "../../../public/certification/fm_17.png";
-import fm16 from "../../../public/certification/certified_16_logo_4clr.jpg";
-import fm18 from "../../../public/certification/certified_18_logo_rev.png";
-import fm2020 from "../../../public/certification/Claris-Developer-Badge-White.png";
+import { certifications } from "../backend/certifications/certifications";
 
 type Data = [{
   id: number;
@@ -44,73 +29,6 @@ async function getCertifications() {
 
 export default async function Certification() {
   //const certifications: Data = await getCertifications();
-  const certifications = [
-    {
-        "name": "FileMaker16: Developer Essentials for FileMaker 16",
-        "image": fm16
-    },
-    {
-      "name": "FileMaker17: Developer Essentials for FileMaker 17",
-      "image": fm17
-    },
-    {
-      "name": "FileMaker18: Developer Essentials for FileMaker 18",
-      "image": fm18
-    },
-    {
-      "name": "FileMaker2020: Developer Essentials for Claris FileMaker",
-      "image": fm2020
-    },
-    {
-      "name": "Developer Essentials for Claris FileMaker",
-      "image": defmp
-    },
-    {
-      "name": "App Developer for Claris FileMaker Pro Associate",
-      "image": fmpa
-    },
-    {
-      "name": "App Developer for Claris Pro Specialist",
-      "image": fmps
-    },
-    {
-      "name": "App Developer for Claris Connect Specialist",
-      "image": ccs
-    },
-    {
-      "name": "Claris FileMaker Server Administrator Associate",
-      "image": fmsa
-    },
-    {
-      "name": "App Developer for Claris Connect Expert",
-      "image": ccexpert
-    },
-    {
-      "name": "App Developer for Claris Connect Associate",
-      "image": cca
-    },
-    {
-      "name": "Salesforce Certified Administrator (SCA)",
-      "image": ""
-    },
-    {
-      "name": "Tableau Certified Data Analyst",
-      "image": da
-    },
-    {
-      "name": "Tableau Server Certified Associate",
-      "image": tsa
-    },
-    {
-      "name": "Tableau Certified Associate Consultant",
-      "image": tcc
-    },
-    ,
-    {
-      "name": "Microsoft Certified: Azure Fundamentals",
-      "image": az900
-    }
-];
 
   return (
     <>
--- src/app/skills/page.tsx
+++ src/app/skills/page.tsx
@@ -1,15 +1,5 @@
 import Image from "next/image";
-import aws from "../../../public/skills/aws.png";
-import css from "../../../public/skills/css.png";
-import firebase from "../../../public/skills/firebase.png";
-import github from "../../../public/skills/github.png";
-import html from "../../../public/skills/html.png";
-import javascript from "../../../public/skills/javascript.png";
-import mongo from "../../../public/skills/mongo.png";
-import nextjs from "../../../public/skills/nextjs.png";
-import node from "../../../public/skills/node.png";
-import tailwind from "../../../public/skills/tailwind.png";
-import react from "../../../public/skills/react.png";
+import { skills } from "../backend/skills/skills";
 
 type Data = [{
     id: number;
@@ -30,52 +20,6 @@ async function getSkills() {
 
 export default async function Skills() {
     //const skills: Data = await getSkills();
-    const skills = [
-        {
-            "name": "html",
-            "image": html
-        },
-        {
-            "name": "css",
-            "image": css
-        },
-        {
-            "name": "javascript",
-            "image": javascript
-        },
-        {
-            "name": "react",
-            "image": react
-        },
-        {
-            "name": "nextjs",
-            "image": nextjs
-        },
-        {
-            "name": "tailwind",
-            "image": tailwind
-        },
-        {
-            "name": "github",
-            "image": github
-        },
-        {
-            "name": "node",
-            "image": node
-        },
-        {
-            "name": "mongo",
-            "image": mongo
-        },
-        {
-            "name": "aws",
-            "image": aws
-        },
-        {
-            "name": "firebase",
-            "image": firebase
-        }
-    ];
 
     return (
         <>
